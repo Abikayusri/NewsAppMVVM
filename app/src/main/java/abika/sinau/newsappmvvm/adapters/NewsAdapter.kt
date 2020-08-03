f286036de557e4b02fe4a948cca1b0ef02657a87
Abika Chairul Yusri <35727186+Abikayusri@users.noreply.github.com>
2020-08-03 11:52:16 +0700

    Finish Material RecyclerView with DiffUtil

--- app/src/main/java/abika/sinau/newsappmvvm/adapters/NewsAdapter.kt
+++ app/src/main/java/abika/sinau/newsappmvvm/adapters/NewsAdapter.kt
@@ -17,22 +17,30 @@ import kotlinx.android.synthetic.main.item_article_preview.view.*
  * on Sunday, 28 June 2020
  * Bismillahirrahmanirrahim
  */
+// TODO 6-1: Buat sebuah class adapters
 class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
 
     inner class ArticleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
 
+    // TODO 6-2: buat sebuah diffUtils. DiffUtils berfungsi untuk menampilkan data dengan mudah
+    //  agar tidak terjadi double data saat ditampilkan
     private val differCallback = object : DiffUtil.ItemCallback<ArticlesItem>() {
+        // ketika item sama. masukkan sesuatu yang unik yang memiliki isi sama seperti id/url
         override fun areItemsTheSame(oldItem: ArticlesItem, newItem: ArticlesItem): Boolean {
             return oldItem.url == newItem.url
         }
 
+        // membandingkan content ketika terjadi kesamaan
         override fun areContentsTheSame(oldItem: ArticlesItem, newItem: ArticlesItem): Boolean {
             return oldItem == newItem
         }
     }
 
+    // TODO 6-3: Masukkan async list differ. list differ merupakan tools yang digunakan untuk membandingkan
+    //  2 list dan mengkalkulasikan perbedaannya. karena async ini akan dijalankan di background
     val differ = AsyncListDiffer(this, differCallback)
 
+    // TODO 6-4: Implement object seperti biasa
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
         return ArticleViewHolder(
             LayoutInflater.from(parent.context).inflate(
@@ -44,9 +52,11 @@ class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
     }
 
     override fun getItemCount(): Int {
+        // karena kita menggunakan differ. kita panggil isi list differ di sini
         return differ.currentList.size
     }
 
+    // TODO 6-5: Buat sebuah fungsi ketika item diklik
     private var onItemClickListener: ((ArticlesItem) -> Unit)? = null
 
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
@@ -58,6 +68,7 @@ class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
             tvDescription.text = article.description
             tvPublishedAt.text = article.publishedAt
 
+            // TODO 6-5: Buat sebuah fungsi ketika item diklik
             setOnClickListener {
                 onItemClickListener?.let { it(article) }
             }
@@ -67,5 +78,4 @@ class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
     fun setOnItemClickListener(listener: (ArticlesItem) -> Unit) {
         onItemClickListener = listener
     }
-
 }
